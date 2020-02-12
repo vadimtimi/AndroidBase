2674361d9062fdc38083ac1609c0e79eeeb1920f
VadimTimofeev <timofeev@iskra-kkm.ru>
2020-02-12 19:21:12 +0300

    - Home 02

---
+++ app/src/main/java/com/refresh/pos/telegabot/frm_settings.java
@@ -0,0 +1,49 @@
+package com.refresh.pos.telegabot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class frm_settings extends AppCompatActivity implements View.OnClickListener  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_settings);
+
+        // Доступ к переменным другого класса
+        MainActivity.telegrammToken = "";
+        MainActivity.telegrammUser= "";
+
+        Button cmdSave = findViewById(R.id.cmdSave);
+        cmdSave.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cmdSave:
+
+                EditText txtToken = findViewById(R.id.txtToken);
+                EditText txtUser = findViewById(R.id.txtUser);
+                // Доступ к переменным другого класса
+
+
+                MainActivity.telegrammToken = txtToken.getText().toString().trim();
+                MainActivity.telegrammUser= txtUser.getText().toString().trim();
+
+                Toast.makeText(this, "Settings Save!", Toast.LENGTH_SHORT).show();
+
+                finish();
+
+                return;
+            default:
+                return;
+        }
+    }
+
+}
